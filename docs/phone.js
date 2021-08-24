3df5f8d212a7a4e07289603c10098057ddfaa379
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-08-24 10:54:20 -0700

    adding the docs folder

---
+++ docs/phone.js
@@ -0,0 +1,14 @@
+
+
+$(function () {
+
+    $(window).on('scroll', function()  {
+
+        if($(window).scrollTop() > 10   ) {
+            $('.navbar').addClass ('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+
+});
